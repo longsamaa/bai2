ed86f60d6f2a810fb7438b9bfdedf6186f18ee4d
= <=>
2020-04-15 09:37:07 +0700

    Updates

---
+++ precache-manifest.234f4dbe8a1143d17f772d40bc5daa8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92539c309edd3aca685ddaf365f62a0b",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "bae44b3a2ecd734308b8",
+    "url": "/myapp/static/css/main.c52fc0cb.chunk.css"
+  },
+  {
+    "revision": "7d44110e9f4f61708169",
+    "url": "/myapp/static/js/2.0ddc7b85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.0ddc7b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae44b3a2ecd734308b8",
+    "url": "/myapp/static/js/main.826cee20.chunk.js"
+  },
+  {
+    "revision": "73612b6c0e9b147867e0",
+    "url": "/myapp/static/js/runtime-main.cb85d09e.js"
+  }
+]);
